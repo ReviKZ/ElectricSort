8990d0f43b1028cfbe93560a172efa246c8ac636
ReviKZ <gameplant01@gmail.com>
2024-11-27 09:28:04 +0100

    Finish required methods

---
+++ ElectricSort/Classes/Select.cs
@@ -0,0 +1,69 @@
+﻿namespace ElectricSort.Classes
+{
+    /// <summary>
+    /// Tömb műveletek osztálya amely valamilyen szempont alapján kiválaszt egy autót. [Statikus osztály, ezért nincs szükség konstruktorra mert nem példányosítható]
+    /// </summary>
+    public static class Select
+    {
+        /// <summary>
+        /// A tömb legnagyobb eleme akkumulátor kapacitás alapján. 
+        /// </summary>
+        /// <param name="array">A tömb amiből választjuk a legnagyobbat.</param>
+        /// <returns>A rendezett lista</returns>
+        public static ElectricCar MaxByCapacity(ElectricCar[] array)
+        {
+            ElectricCar? max = array[0]; // Kezdőérték a 0. indexen
+
+            for (var i = 1; i < array.Length; i++) // Mivel a 0. indexű elemmel kezdünk, az kihagyható az összehasonlításból
+            {
+                if (array[i].BatteryCapacity > max.BatteryCapacity) // Ha az akksi kapacitás nagyobb a vizsgáltnak
+                {
+                    max = array[i]; // Beállítás új értékként.
+                }
+            }
+
+            return max;
+        }
+
+        /// <summary>
+        /// A tömb legnagyobb eleme hatótáv alapján. 
+        /// </summary>
+        /// <param name="array">A tömb amiből választjuk a legnagyobbat.</param>
+        /// <returns>A rendezett lista</returns>
+        public static ElectricCar MaxByRange(ElectricCar[] array)
+        {
+            ElectricCar? max = array[0]; // Kezdőérték a 0. indexen
+
+            for (var i = 1; i < array.Length; i++) // Mivel a 0. indexű elemmel kezdünk, az kihagyható az összehasonlításból
+            {
+                if (array[i].RealRange > max.RealRange) // Ha a hatótáv alapján nagyobb a vizsgáltnak
+                {
+                    max = array[i]; // Beállítás új értékként.
+                }
+            }
+
+            return max;
+        }
+
+        /// <summary>
+        /// A tömb legnagyobb eleme hatótáv alapján. 
+        /// </summary>
+        /// <param name="array">A tömb amiből választjuk a legnagyobbat.</param>
+        /// <returns>A rendezett lista</returns>
+        public static decimal AverageAgeOfCars(ElectricCar[] array)
+        {
+            decimal sumOfAge = 0; // Az autók életkorának összege
+            DateTime today = DateTime.Today; // Veszi a mai dátumot
+
+            for (var i = 0; i < array.Length; i++)
+            {
+                decimal age = today.Year - array[i].ModelYear.Year; // Kivonja a mai dátumból az autó évjáratát, hogy megapja a korát
+                sumOfAge += age; // Hozzáadja az össz életkorhoz.
+            }
+
+            decimal avgAge = sumOfAge / array.Length; // Az össz életkort elosztja a tömb elemeinek számával, hogy megkapja az átlagéletkort.
+
+            return avgAge;
+        }
+    }
+}
--- ElectricSort/Program.cs
+++ ElectricSort/Program.cs
@@ -3,7 +3,7 @@ using ElectricSort.Enums;
 
 bool running = true; // A program futásának reprezentációja.
 string? message = null; // Egy üzenet, ami opcionálisan megjelenhet.
-ElectricCar[] electricCars = Sort.InitializeArray();
+ElectricCar[] electricCars = Sort.InitializeArray(); // Az elektromos autók tömbjének feltöltése.
 
 while (running) // Végtelen ciklus, amíg a program fut.
 {
@@ -14,6 +14,8 @@ while (running) // Végtelen ciklus, amíg a program fut.
             [1] - Autók listázása a legutóbbi rendezés sorrendjében
             [2] - Autók rendezése szempont alapján
             [3] - A lista alaphelyzetbe állítása
+            [4] - A legnagyobb értékkel rendelkező autó kiválasztása egy szempont alapján.
+            [5] - A tömbben szereplő autók átlagéletkora.
             [9] - Kilépés 
 
             {message ?? "" /* Ha nincs hibaüzenet akkor üres */} 
@@ -103,18 +105,18 @@ while (running) // Végtelen ciklus, amíg a program fut.
                 {errorMessage ?? "" /* Ha nincs hibaüzenet akkor üres */}
                 """);
                 Console.Write("> ");
-                string? inputTwo = Console.ReadLine();
+                string? inputTwo = Console.ReadLine(); // Kéri az inputot
                 if (!int.TryParse(inputTwo, out int optionTwo)) // Ha nem számot írtunk, hibaüzenet
                 {
                     errorMessage = "Kérem írjon be egy számot!";
                     continue;
                 }
-                else if (optionTwo < 1 || optionTwo > 2)
+                else if (optionTwo < 1 || optionTwo > 2) // Ha nem a listából választunk, hibaüzenet
                 {
                     errorMessage = "Kérem a listából válasszon!";
                     continue;
                 }
-                else
+                else // Egyébként megyünk tovább hibaüzenet nélkül
                 {
                     errorMessage = null;
                     options[1] = optionTwo;
@@ -158,6 +160,89 @@ while (running) // Végtelen ciklus, amíg a program fut.
             electricCars = Sort.InitializeArray();
             message = "A lista alaphelyzetbe állítva!";
             continue;
+        }
+        else if (selection == 4) // Ha 4-es, almenü
+        {
+            string? errorMessage = null; // Opcionális hibaüzenet.
+            int? option = null;
+
+            while (option == null) // Amíg nincs választott érték.
+            {
+                Console.Clear();
+                Console.WriteLine
+                    ($"""
+                        Kérem válasszon melyik szempont alapján kívánja megtalálni a legnagyobb elemet:
+                        [1] - Akkumulátor kapacitás alapján
+                        [2] - Hatótáv alapján
+
+                        {errorMessage ?? "" /* Ha nincs hibaüzenet akkor üres */} 
+                    """);
+
+                Console.Write("> ");
+                string? optionString = Console.ReadLine(); // Kéri az inputot
+                if (!int.TryParse(optionString, out int selectedOption)) // Ha nem számot írtunk, hibaüzenet
+                {
+                    errorMessage = "Kérem írjon be egy számot!";
+                    continue;
+                }
+                else if (selectedOption < 1 || selectedOption > 2) // Ha nem a listából választunk, hibaüzenet
+                {
+                    errorMessage = "Kérem a listából válasszon!";
+                    continue;
+                }
+                else // Egyébként megyünk tovább hibaüzenet nélkül
+                {
+                    errorMessage = null;
+                    option = selectedOption;
+                    break;
+                }
+            }
+
+            if (option == 1) // Ha az első opció
+            {
+                string? goBack = null;
+                while (goBack == null) // Amíg nincs visszatérési érték
+                {
+                    Console.Clear();
+                    ElectricCar car = Select.MaxByCapacity(electricCars); // Megfelelő metódus meghívása.
+                    Console.WriteLine($"""
+                    A legnagyobb akkumulátorkapacitással rendelkező autó:
+                    {car.ToString()}
+                    """);
+                    Console.WriteLine();
+                    Console.WriteLine("Ha vissza akar menni a menübe, kérem nyomjon egy ENTER-t!");
+                    goBack = Console.ReadLine(); // Várja az inputot
+                }
+            }
+
+            if (option == 2) // Ha a második opció
+            {
+                string? goBack = null;
+                while (goBack == null) // Amíg nincs visszatérési érték
+                {
+                    Console.Clear();
+                    ElectricCar car = Select.MaxByRange(electricCars); // Megfelelő metódus meghívása.
+                    Console.WriteLine($"""
+                    A legnagyobb hatótávval rendelkező autó:
+                    {car.ToString()}
+                    """);
+                    Console.WriteLine();
+                    Console.WriteLine("Ha vissza akar menni a menübe, kérem nyomjon egy ENTER-t!");
+                    goBack = Console.ReadLine();
+                }
+            }
+        }
+        else if (selection == 5) // Ha az ötös
+        {
+            Console.Clear();
+            decimal avgAge = Select.AverageAgeOfCars(electricCars); // Megfelelő metódus meghívása.
+            Console.WriteLine($"""
+                    A tömbben szereplő autók átlagéletkora:
+                    {avgAge}
+                    """);
+            Console.WriteLine();
+            Console.WriteLine("Ha vissza akar menni a menübe, kérem nyomjon egy ENTER-t!");
+            Console.ReadLine();
         }
         else // Ha más szám, hibaüzenet
         {
