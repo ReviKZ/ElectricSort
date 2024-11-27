a27aae5c59d921251a166bcae396adc4b45a30c5
ReviKZ <gameplant01@gmail.com>
2024-11-27 09:39:13 +0100

    Remove clutter;

--- ElectricSort/Classes/ElectricCar.cs
+++ ElectricSort/Classes/ElectricCar.cs
@@ -1,9 +1,4 @@
 ﻿using ElectricSort.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ElectricSort.Classes
 {
